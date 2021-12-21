1cfe4d298b56ea8d7c8b49d85dcdc50f51f479fb
Francisco José Rodríguez Bogado <bogado@qinn.es>
2021-12-21 19:33:58 +0100

    Testing PDM.

---
+++ toodles.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+"""
+Toodles
+=======
+
+Lee el peso de la báscula por puerto serie y la escribe en un fichero.
+"""
+
+import sys
+
+def main():
+    """
+    Rutina principal.
+    """
+    print("Hola holita, vecinito.")
+    sys.exit(0)
+
+if __name__ == "__main__":
+    main()
